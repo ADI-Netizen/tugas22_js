c6049b38068403ed63ecbfb07d1ec4ff5a1d75b6
ADI-Netizen <71365604+ADI-Netizen@users.noreply.github.com>
2020-09-22 07:31:10 +0700

    Add files via upload

---
+++ tugas22.js
@@ -0,0 +1,8 @@
+function duadua() {
+    var kalimat = ["Saya", "ingin", "belajar", "bersama"]
+    kalimat.forEach(function (item, index) {
+        console.log("Item :", item, "Index ke ", index)
+    })
+}
+
+duadua()
